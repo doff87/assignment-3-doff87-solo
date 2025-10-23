a213fe699da8f26520d0d013e05df13a1a68e677
doff87 <s-david.offutt@lwtech.edu>
2025-10-22 23:55:19 -0700

    Add test_main.py and begin populating tests

---
+++ test_main.py
@@ -0,0 +1,18 @@
+from main import calculate_total, calculate_average, find_max, find_min
+import pytest
+
+itemsInts = [1, 2, 3]
+itemsFloats = [1.0, 2.0, 3.0]
+itemsEmpty = []
+itemsNone = None
+itemsStrings = ["Hello", "Hi", "Howdy"]
+
+
+
+def test_calculate_total_ints():
+    """Total of [1, 2, 3] should be 6"""
+    #arrange
+    itemsInts
+
+    #
+
